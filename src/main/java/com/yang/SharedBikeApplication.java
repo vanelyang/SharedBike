9895ac56799eb7d59d99bb2e90c94489646fbd74
yang <vanelyang@163.com>
2024-03-17 17:29:12 +0800

    创建项目

---
+++ src/main/java/com/yang/SharedBikeApplication.java
@@ -0,0 +1,13 @@
+package com.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedBikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedBikeApplication.class, args);
+    }
+
+}
